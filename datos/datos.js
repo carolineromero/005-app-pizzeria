fb5821780f9eb1b46e23556e43b38571eb54ad51
mflamanna <lamanna.mflorencia@gmail.com>
2022-06-07 09:59:19 +0200

    pizza item

---
+++ datos/datos.js
@@ -0,0 +1,74 @@
+arrayPizzas = [
+    {
+     imgPizza: "./images/pizza1.png",
+     nomPizza: "Peperoni",
+     ingredientePpal: "Peperoni y queso",
+     precioPizza: 18,
+    },
+    {
+        imgPizza: "./images/pizza2.png",
+        nomPizza: "Napolitana",
+        ingredientePpal: "Queso",
+        precioPizza: 18,
+    },
+    {
+        imgPizza: "./images/pizza3.png",
+        nomPizza: "Cuatro Quesos",
+        ingredientePpal: "Queso Emental",
+        precioPizza: 20,
+    },
+    {
+        imgPizza: "./images/pizza4.png",
+        nomPizza: "Margarita",
+        ingredientePpal: "Queso",
+        precioPizza: 20,
+    },
+    {
+        imgPizza: "./images/pizza5.png",
+        nomPizza: "Hawaiana",
+        ingredientePpal: "Jamón dulce y queso",
+        precioPizza: 22,
+    },
+    {
+        imgPizza: "./images/pizza6.png",
+        nomPizza: "Veggetariana",
+        ingredientePpal: "Calabacin y cebolla",
+        precioPizza: 22,
+    },
+    {
+        imgPizza: "./images/pizza7.png",
+        nomPizza: "Peperoni Supreme",
+        ingredientePpal: "Doble peperoni",
+        precioPizza: 20,
+    },
+    {
+        imgPizza: "./images/pizza8.png",
+        nomPizza: "Napolitana Supreme",
+        ingredientePpal: "Doble queso",
+        precioPizza: 20,
+    },
+    {
+        imgPizza: "./images/pizza9.png",
+        nomPizza: "Cuatro Quesos Supreme",
+        ingredientePpal: "Doblequeso emental",
+        precioPizza: 22,
+    },
+    {
+        imgPizza: "./images/pizza10.png",
+        nomPizza: "Margarita Supreme",
+        ingredientePpal: "Doble queso",
+        precioPizza: 22,
+    },
+    {
+        imgPizza: "./images/pizza11.png",
+        nomPizza: "Hawaiana Supreme",
+        ingredientePpal: "Doble Jamón Dulce y Queso",
+        precioPizza: 24,
+    },
+    {
+        imgPizza: "./images/pizza12.png",
+        nomPizza: "Veggetariana Supreme",
+        ingredientePpal: "Doble Calabacín y Queso",
+        precioPizza: 24,
+    }
+]
