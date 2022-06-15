9fbf5ace69ddd080869b0ab9afc758808087a6ee
GabyBarajitas <gabybarajasbcn@gmail.com>
2022-06-16 00:38:46 +0200

    nueva version

--- datos/datos.js
+++ datos/datos.js
@@ -1,110 +1,90 @@
-arrayPizzas = [
+let arrayPizzas = [
     {
-     imgPizza: "./images/pizza1.png",
-     nomPizza: "Peperoni",
-     ingredientePpal: "Peperoni y queso",
-     precioPizza: 18,
-     cantidad: 0
+        idPizza: 0,
+        imgPizza: "./images/pizza1.png",
+        nomPizza: "Peperoni",
+        ingredientePpal: "Peperoni y queso",
+        precioPizza: 18
     },
     {
+        idPizza: 1,
         imgPizza: "./images/pizza2.png",
         nomPizza: "Napolitana",
         ingredientePpal: "Queso",
-        precioPizza: 18,
-        cantidad: 0
+        precioPizza: 18
+        
     },
     {
+        idPizza: 2,
         imgPizza: "./images/pizza3.png",
         nomPizza: "Cuatro Quesos",
         ingredientePpal: "Queso Emental",
-        precioPizza: 20,
-        cantidad: 0
+        precioPizza: 20
     },
     {
+        idPizza: 3,
         imgPizza: "./images/pizza4.png",
         nomPizza: "Margarita",
         ingredientePpal: "Queso",
-        precioPizza: 20,
-        cantidad: 0
+        precioPizza: 20
     },
     {
+        idPizza: 4,
         imgPizza: "./images/pizza5.png",
         nomPizza: "Hawaiana",
         ingredientePpal: "Jamón dulce y queso",
-        precioPizza: 22,
-        cantidad: 0
+        precioPizza: 22
     },
     {
         imgPizza: "./images/pizza6.png",
         nomPizza: "Veggetariana",
         ingredientePpal: "Calabacin y cebolla",
         precioPizza: 22,
-        cantidad: 0
+        idPizza: 5
     },
     {
+        idPizza: 6,
         imgPizza: "./images/pizza7.png",
         nomPizza: "Peperoni Supreme",
         ingredientePpal: "Doble peperoni",
-        precioPizza: 20,
-        cantidad: 0
+        precioPizza: 20
+        
     },
     {
+        idPizza: 7,
         imgPizza: "./images/pizza8.png",
         nomPizza: "Napolitana Supreme",
         ingredientePpal: "Doble queso",
-        precioPizza: 20,
-        cantidad: 0
+        precioPizza: 20
     },
     {
+        idPizza: 8,
         imgPizza: "./images/pizza9.png",
         nomPizza: "Cuatro Quesos Supreme",
         ingredientePpal: "Doblequeso emental",
-        precioPizza: 22,
-        cantidad: 0
+        precioPizza: 22
     },
     {
+        idPizza: 9,
         imgPizza: "./images/pizza10.png",
         nomPizza: "Margarita Supreme",
         ingredientePpal: "Doble queso",
-        precioPizza: 22,
-        cantidad: 0
+        precioPizza: 22
+        
     },
     {
+        idPizza: 10,
         imgPizza: "./images/pizza11.png",
         nomPizza: "Hawaiana Supreme",
         ingredientePpal: "Doble Jamón Dulce y Queso",
-        precioPizza: 24,
-        cantidad: 0
+        precioPizza: 24
     },
     {
+        idPizza: 11,
         imgPizza: "./images/pizza12.png",
         nomPizza: "Veggetariana Supreme",
         ingredientePpal: "Doble Calabacín y Queso",
         precioPizza: 24,
-        cantidad: 0
     }
 ]
-
-arrayPizzasSeleccionadas = [
-    {
-     imgPizza: "./images/pizza1.png",
-     nomPizza: "Peperoni",
-     ingredientePpal: "Peperoni y queso",
-     precioPizza: 18,
-     cantidad: 8
-    },
-    {
-        imgPizza: "./images/pizza2.png",
-        nomPizza: "Napolitana",
-        ingredientePpal: "Queso",
-        precioPizza: 18,
-        cantidad: 3
-    },
-    {
-        imgPizza: "./images/pizza3.png",
-        nomPizza: "Cuatro Quesos",
-        ingredientePpal: "Queso Emental",
-        precioPizza: 20,
-        cantidad: 4
-    },
-]
+let carritoCompra = [];
--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,112 +1,94 @@
-function ocultarPrincipal() {
-  document.querySelector(".paginaPrincipal").style.display = "none";
-  document.querySelector(".paginaCarrito").style.display = "block";
+const posicionPizzas = document.querySelector(".pizza-item-list");
+const posicionCarrito = document.querySelector(".item-selec");
+
+//function ocultarPrincipal() {
+  //document.querySelector(".paginaPrincipal").style.display = "none";
+  //document.querySelector(".paginaCarrito").style.display = "block";
+//}
+
+//function mostrarPrincipal() {
+  //document.querySelector(".paginaPrincipal").style.display = "block";
+  //document.querySelector(".paginaCarrito").style.display = "none";
+//}
+
+function renderizaPizzas(){
+  arrayPizzas.forEach((itemPizza)=>{
+    posicionPizzas.innerHTML +=
+  `
+  <div class="item-pizza">
+      <div class="padre">
+      <div class="marco-pizza-img">
+          <img class = 'img-pizza' src='${itemPizza.imgPizza}'>
+      </div>
+      </div>
+      <div class="texto-pizza">
+          <div class="pizza-name">
+              <p class="nombre">${itemPizza.nomPizza}</p>  
+              <p class="ingredientes">${itemPizza.ingredientePpal}</p>  
+          </div>
+      <div class="pizza-precio">
+          <p class = "precio" ><span>$</span>${itemPizza.precioPizza}</p>
+          <button class = "button-add" onclick= "sumaPizza(${itemPizza.idPizza})">+</button>
+      </div>
+      </div> 
+  </div>
+  `
+})
+} 
+
+function sumaPizza(indiceElemento){
+  if (carritoCompra.some(pizzaSel => pizzaSel.idPizza === indiceElemento)){
+    alert("Pizza ya existe");
+  }else{
+    const pizzaSel = arrayPizzas.find((pizza)=> pizza.idPizza === indiceElemento );
+    carritoCompra.push({
+      ...pizzaSel,
+      unidades: 1
+    })
+    renderizaCarritoCompra();
+  }
 }
 
-function mostrarPrincipal() {
+function renderizaCarritoCompra(){
+  posicionCarrito.innerHTML = "";
+
+  carritoCompra.forEach((pizzaSel) => {
+    posicionCarrito.innerHTML +=
+    `
+    <div class = "item-selec-info">
+      <div class = "img-precio-nombre">
+        <img src="${pizzaSel.imgPizza}" class = "item-img-pizza" alt="Pizza seleccionada">
+        <p class = "item-selec-info-precio"><span>$</span>${pizzaSel.precioPizza}</p>
+        <p class = "item-selec-info-nombre">${pizzaSel.nomPizza}</p>
+      </div>
+      <div class = "item-selec-cantidad">
+        <p class ="buttn-item-selec-suma" onclick "cambiaUnidades('mas',${pizzaSel.idPizza})">+</p>
+        <p class ="item-selec-info-cantidad">${pizzaSel.unidades}</p>
+        <p class ="buttn-item-selec-resta" cambiaUnidades('menos',${pizzaSel.idPizza})">-</p>
+      </div>
+    </div> `
+  }) 
+}
+
+function cambiaUnidades(accion,id){
+  carritoCompra = carritoCompra.map((pizzaSel) =>{
+
+    if (pizzaSel.idPizza === id){
+      
+    }
+
+    return pizzaSel;
+
+  })
+
+}
+
+
+
+function main(){
   document.querySelector(".paginaPrincipal").style.display = "block";
-  document.querySelector(".paginaCarrito").style.display = "none";
+  document.querySelector(".paginaCarrito").style.display = "block";
+  renderizaPizzas()
 }
 
-function newNodePizza(newImg,newName,newIngred,newPrice, indiceElemento){
-    let image = newImg;
-    let nombre = newName;
-    let ingrediente = newIngred;
-    let precio = newPrice;
-    let nodoNuevo =
-    `
-    <div class="item-pizza">
-        <div class="padre">
-        <div class="marco-pizza-img">
-            <img class = 'img-pizza' src='${image}'>
-        </div>
-        </div>
-        <div class="texto-pizza">
-            <div class="pizza-name">
-                <p class="nombre">${nombre}</p>  
-                <p class="ingredientes">${ingrediente}</p>  
-            </div>
-        <div class="pizza-precio">
-            <p class = "precio" ><span>$</span>${precio}</p>
-            <button class = "button-add" onclick= "sumarPizza(${indiceElemento})">+</button>
-        </div>
-        </div> 
-    </div>
-    `
-
-    return nodoNuevo;
-}
-
-function insertNodePizza(nodeInfo){
-    let posicion = document.querySelector(".pizza-item-list");
-    posicion.innerHTML = nodeInfo;
-  }
-function despliegaMenu(){
-  let concatNodes = ``;
-  let image;
-  let nombre;
-  let ingrediente;
-  let precio;
-  let sizeArr = arrayPizzas.length;;
-  let i = 0;
-  for (i; i < sizeArr; i++ ){
-    image = arrayPizzas[i].imgPizza;
-    nombre = arrayPizzas[i].nomPizza;
-    ingrediente = arrayPizzas[i].ingredientePpal;
-    precio = arrayPizzas[i].precioPizza;
-    concatNodes = concatNodes + newNodePizza(image,nombre,ingrediente,precio,i);
-  }
-  insertNodePizza(concatNodes);
-}
-despliegaMenu();
-
-function sumarPizza (indiceElemento){
-  arrayPizzas[indiceElemento].cantidad++
-  console.log (arrayPizzas[indiceElemento])
-}
-
-function newNodePizzaSeleccionada(newImg,newName,newPrice,indiceElemento){
-  let image = newImg;
-  let nombre = newName;
-  let precio = newPrice;
-  let cantidad = arrayPizzasSeleccionadas[indiceElemento.cantidad]
-  let nodoNuevo =
-  `
-  <div class = "item-selec-info">
-        <div class = "img-precio-nombre">
-        <img src="${image}" class = "item-img-pizza" alt="Pizza seleccionada">
-        <p class = "item-selec-info-precio"><span>$</span>${precio}</p>
-        <p class = "item-selec-info-nombre">${nombre}</p>
-    </div>
-    <div class = "item-selec-cantidad">
-        <p class ="buttn-item-selec-suma">+</p>
-        <p class ="item-selec-info-cantidad">${cantidad}</p>
-        <p class ="buttn-item-selec-resta">-</p>
-    </div>
-    </div>
-  `
-  return nodoNuevo;
-}
-
-function insertNodePizzaSeleccionada(nodeInfo){
-  let posicion = document.querySelector(".item-selec");
-  posicion.innerHTML = nodeInfo;
-}
-function despliegaCarrio(){
-let concatNodes = ``;
-let image;
-let nombre;
-let ingrediente;
-let precio;
-let sizeArr = arrayPizzas.length;;
-let i = 0;
-for (i; i < sizeArr; i++ ){
-  image = arrayPizzas[i].imgPizza;
-  nombre = arrayPizzas[i].nomPizza;
-  ingrediente = arrayPizzas[i].ingredientePpal;
-  precio = arrayPizzas[i].precioPizza;
-  concatNodes = concatNodes + newNodePizza(image,nombre,ingrediente,precio,i);
-}
-insertNodePizza(concatNodes);
-}
-despliegaMenu();
+main();
